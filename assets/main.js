40e5d32827dafac7c705d19629f25090b3576900
Guilherme Alves <guieduardoalves@gmail.com>
2026-05-28 15:40:40 -0300

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -5,6 +5,7 @@ let local = document.getElementById("local")
 let data = document.getElementById("data")
 let btn = document.querySelector(".btn")
 let copiarBtn = document.querySelector(".copiar")
+const limparBtn = document.querySelector(".limpar");
 let resultado = document.getElementById("resultado")
 let mensagem = document.getElementById("mensagem")
 
@@ -29,7 +30,7 @@ function gerarCSV(sarams, designacao, local, data) {
 // Adiciona um evento de clique ao botão para gerar o CSV
 btn.addEventListener("click", () => {
 
-    // validação
+    // validação dos campos para garantir que todos estão preenchidos
     if (
         saram.value === "" ||
         designacao.value === "" ||
@@ -37,14 +38,25 @@ btn.addEventListener("click", () => {
         data.value === ""
     ) {
 
-        alert(
-            "Todos os campos precisam ser preenchidos"
-        );
+        mensagem.textContent = "Todos os campos precisam ser preenchidos.";
+
+        mensagem.style.color = "#ff4d4d";
+
 
         return;
     }
-
     const saramValor = saram.value.split(/[\s,]+/).filter(item => item !== '');
+
+    // validação para garantir que o campo SARAM contenha apenas números
+    if (saramValor.length === 0 || isNaN(saramValor.join(''))) {
+        msg_saram.textContent = "O campo SARAM deve conter apenas números.";
+        msg_saram.style.color = "#ff4d4d";
+        mensagem.textContent = "";
+        
+        return;
+        
+    }
+
     const designacaoValor = designacao.value;
     const localValor = local.value;
     const dataValor = new Date(data.value)
@@ -53,7 +65,9 @@ btn.addEventListener("click", () => {
     const csv = gerarCSV(saramValor, designacaoValor, localValor, dataValor);
     resultado.textContent = csv;
 
-    //Limpa os inputs após gerar o CSV
+    //Limpa os inputs/mensagens após gerar o CSV
+    msg_saram.textContent = "";
+    mensagem.textContent = "";
     saram.value = "";
     designacao.value = "";
     local.value = "";
@@ -76,5 +90,14 @@ copiarBtn.addEventListener("click", () => {
         navigator.clipboard.writeText(resultado.textContent);
         mensagem.textContent = "CSV copiado para a área de transferência!";
         mensagem.style.color = "#4CAF50";
+    }
+});
+
+// Adiciona um evento de clique ao botão para limpar o CSV
+limparBtn.addEventListener("click", () => {
+    if (resultado.textContent !== "") {
+        resultado.textContent = "";
+        mensagem.textContent = "CSV limpo.";
+        mensagem.style.color = "#fdf6f6";
     }
 });
